b483ea7c9e9469dec54de036070c7191aca2dce9
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2020-12-15 17:08:30 -0800

    uploading all package files

---
+++ server.js
@@ -0,0 +1,19 @@
+// const express = require("express");
+// const app = express()
+
+// var PORT = process.env.PORT || 3001;
+
+// app.get("/", function(req, res) {
+//     res.json(path.join(__dirname, "public/index.html"));
+//   });
+
+//   app.post("/api/reserve/", function(req, res) {
+//     console.log(req.body)
+//   });
+  
+ 
+// app.listen(PORT, function(){
+
+
+// })
+// console.log("server.js")
